eee611e810ec471dc24d932f68564c4b758c2178
danielbasz <danielbassporto@gmail.com>
2022-03-07 05:00:38 -0500

    ActionDriver2 updated. Part 3 is go.

--- ActionDriver2.java
+++ ActionDriver2.java
@@ -2,8 +2,27 @@
 public class ActionDriver2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		 boolean answer = true;
+		 
+		// If Action.class is not the Superclass of RegularAction.class, answer is now false
+			if (OccasionalAction.class.getSuperclass() != Action.class) {
+				answer = false;
+			}
+			
+			//Outputs are expected to be true for Action.class as superclass of OccaisonalAction.class
+			System.out.println("OccaisonalAction is just a subclass of Action: " + answer);
+			System.out.println("Expected: true");
 
+			// If Action.class is not the Superclass of RegularAction.class, answer is now false
+				if (RareAction.class.getSuperclass() != Action.class) {
+					answer = false;
+				}
+						
+				//Outputs are expected to be true for Action.class as superclass of RareAction.class
+				System.out.println("RareAction is just a subclass of Action: " + answer);
+				System.out.println("Expected: true");	
+			
 	}
 
 }
